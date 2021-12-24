4c8dec911230beec5fb7139b55eb7b500390f7ff
noof99 <93866938+noof99@users.noreply.github.com>
2021-12-24 22:47:03 +0400

    Add files via upload

---
+++ python/equal.py
@@ -0,0 +1,13 @@
+# Relational and Equality Operators
+
+a=30
+
+b=20
+
+if a==b:
+
+  print('a is equal to b')
+
+else:
+
+  print('a and b are not equal')
---
+++ python/num10.py
@@ -0,0 +1,7 @@
+
+import math
+math.sqrt(100)
+num = 10
+math.sqrt(num)
+print(num)
+print(
